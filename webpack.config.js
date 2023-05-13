dbbc8162d0cd18831340b0c5d43a974285de12b6
cookie <0cookieboy0@gmail.com>
2023-05-13 15:44:01 +0900

    init: initialize project, initialize webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 8000,
+    open: true,
+  },
+};
